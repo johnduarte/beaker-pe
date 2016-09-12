bd0d23937ce2261d670ee5c15feb127782e072a9
John Duarte <john.duarte@puppetlabs.com>
2016-09-12 13:28:53 -0700

    (BKR-898) Add OS tests write_client_tool_config_on
    This commit adds spec tests for the write_client_tool_config_on
    to validate that the expected paths are returned based on Windows
    and RHEL host platforms.

--- spec/beaker-pe/pe-client-tools/config_file_helper_spec.rb
+++ spec/beaker-pe/pe-client-tools/config_file_helper_spec.rb
@@ -4,9 +4,41 @@ class MixedWithConfigFileHelper
   include Beaker::DSL::PEClientTools::ConfigFileHelper
 end
 
-describe MixedWithConfigFileHelper do
+class MockResult
+  def initialize(value)
+    @value = value
+  end
+  def stdout
+    return @value
+  end
+end
 
-  it 'has a method to write a config file' do
-    expect(subject.respond_to?('write_client_tool_config_on')).not_to be(false)
+describe MixedWithConfigFileHelper do
+  describe "#write_client_tool_config_on" do
+
+    it 'has a method to write a config file' do
+      expect(subject.respond_to?('write_client_tool_config_on')).not_to be(false)
+    end
+
+    context 'on el-7' do
+      it 'creates file in /etc' do
+        platform =  { :platform => Beaker::Platform.new('el-7-x86_64') }
+        host =  Beaker::Host.create('host', platform, make_host_opts('host', platform))
+        allow(host).to receive(:exec)
+
+        expect(subject).to receive(:create_remote_file).with(host,/^\/etc/, 'some file content')
+        subject.write_client_tool_config_on(host, 'global', 'code', 'some file content')
+      end
+    end
+    context 'on windows' do
+      it 'creates file in C:\programdata' do
+        platform =  { :platform => Beaker::Platform.new('windows-2012r2-x86_64') }
+        host =  Beaker::Host.create('host', platform, make_host_opts('host', platform))
+        allow(host).to receive(:exec).and_return(MockResult.new('C:\ProgramData'))
+
+        expect(subject).to receive(:create_remote_file).with(host,/ProgramData/i, 'some file content')
+        subject.write_client_tool_config_on(host, 'global', 'code', 'some file content')
+      end
+    end
   end
 end
